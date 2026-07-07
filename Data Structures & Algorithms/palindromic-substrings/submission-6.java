fa92f641c40b510b00dd454bab1f7c0db43b3b1c
NitaiMahat <mahatnitai@gmail.com>
2026-07-07 17:31:06 -0500

    Add: palindromic-substrings - submission-6

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-6.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countSubstrings(String s) {
+        if(s.isEmpty()){
+            return 0;
+        }
+      
+        if(s.length()==1){
+            return 1;
+        }
+        int count = 1;
+        for(int i=1;i<s.length();i++){
+            int low = i;
+            int high = i;
+            while(s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+                count++;
+                if(low==-1 || high==s.length()){
+                    break;
+                }
+            }
+             low = i-1;
+            high = i;
+            while(s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+                count++;
+                if(low==-1 || high == s.length() ){
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
