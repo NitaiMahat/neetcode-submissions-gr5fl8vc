8b1e8ed43b4b167bcbc627c9d8fb342b2af76297
NitaiMahat <mahatnitai@gmail.com>
2026-07-08 20:38:32 -0500

    Add: coin-change - submission-11

---
+++ Data Structures & Algorithms/coin-change/submission-11.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+       
+        for(int i=1;i<dp.length;i++){
+            dp[i] = Integer.MAX_VALUE;
+            for(int coin : coins){
+                if(i<coin){
+                    continue;
+                }
+                if(dp[i-coin]!=Integer.MAX_VALUE){
+                     dp[i] = Math.min(dp[i],dp[i-coin]+1);
+                }
+               
+            }
+        }
+    if(dp[amount]==Integer.MAX_VALUE){
+        return -1;
+    }
+    return dp[amount];
+    }
+    
+}
