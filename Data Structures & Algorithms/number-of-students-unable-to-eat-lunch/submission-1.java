fea676e5f7c787b4927be54fae941aaa1e74867c
NitaiMahat <mahatnitai@gmail.com>
2026-07-11 13:53:05 -0500

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> studentsQueue = new LinkedList<>();
+        Stack<Integer> sandwichesStack = new Stack<>();
+        for(int student : students){
+            studentsQueue.add(student);
+        }
+        for(int i=sandwiches.length-1;i>=0;i--){
+            sandwichesStack.push(sandwiches[i]);
+        }
+        int unable = 0;
+        while(!studentsQueue.isEmpty() && unable !=studentsQueue.size()){
+            if(studentsQueue.peek() == sandwichesStack.peek()){
+                studentsQueue.remove();
+                sandwichesStack.pop();
+                unable = 0;
+            }else{
+                int studentsUnable = studentsQueue.remove();
+                studentsQueue.add(studentsUnable);
+                unable++;
+               
+            }
+
+        }
+        
+        return unable;
+
+
+    }
+}
