e89c69e94d81fa03e57a881559f13f524198139d
NitaiMahat <mahatnitai@gmail.com>
2026-07-12 10:08:08 -0500

    Add: time-needed-to-buy-tickets - submission-1

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int target = tickets[k];
+        int time = 0;
+        for(int i=0;i<tickets.length;i++){
+            if(i<=k){
+                time += Math.min(target,tickets[i]);
+            }else{
+                time += Math.min(target-1,tickets[i]);
+            }
+        }
+        return time;
+    }
+}
