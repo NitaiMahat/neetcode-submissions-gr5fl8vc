37e10032cbe38338f70677c5f1a9abe027b3c8a0
NitaiMahat <mahatnitai@gmail.com>
2026-07-19 10:37:37 -0500

    Add: longest-palindromic-substring - submission-10

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-10.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length() <=1){
+            return s;
+        }
+        String ans = "";
+        for(int i=1;i<s.length();i++){
+            int high = i;
+            int low = i;
+            while(s.charAt(low)==s.charAt(high)){
+                high++;
+                low--;
+                if(low<=-1 || high >= s.length()){
+                    break;
+                }
+            }
+            String palindrome = s.substring(low+1,high);
+            if(palindrome.length() > ans.length()){
+                ans = palindrome;
+            }
+
+            low = i-1;
+            high = i;
+
+            while(s.charAt(low)==s.charAt(high)){
+                low--;
+                high++;
+                if(low<=-1 || high >= s.length()){
+                    break;
+                }
+            }
+            palindrome = s.substring(low+1,high);
+            if(palindrome.length() > ans.length()){
+                ans = palindrome;
+            }
+        }
+        return ans;
+
+    }
+}
