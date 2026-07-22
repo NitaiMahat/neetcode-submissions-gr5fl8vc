281783495480fc8f14038898e2db6e0eee5fb6f3
NitaiMahat <mahatnitai@gmail.com>
2026-07-22 15:28:48 -0500

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        intervals.sort((a,b) -> a.start - b.start);
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(Interval interval : intervals){
+            if(!queue.isEmpty() && queue.peek() <= interval.start){
+                queue.poll();
+            }
+            queue.add(interval.end);
+        }
+        return queue.size();
+
+    }
+}
