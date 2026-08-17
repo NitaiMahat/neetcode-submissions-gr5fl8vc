08bd0335d5a60ca60a7821be5d5392a4f8756a38
NitaiMahat <mahatnitai@gmail.com>
2026-08-16 20:16:17 -0500

    Add: buy-and-sell-crypto - submission-13

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-13.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(buyPrice > prices[i]){
+                 buyPrice=prices[i];
+            }
+            maxProfit = Math.max(prices[i]-buyPrice,maxProfit);
+        }
+        return maxProfit;
+    }
+}
